34a860c47d61661e21a466c12008ac3ffd4c0083
Suraj Kumar <77854684+officialsurajkr@users.noreply.github.com>
2022-07-13 19:58:51 +0530

    Update ClassA.java

--- ClassWork02(04-07-22)/PackageProgram/ClassA.java
+++ ClassWork02(04-07-22)/PackageProgram/ClassA.java
@@ -1,7 +1,8 @@
 package pack1;
+//creating ClassA in pack1 package
 public class ClassA
 {
-	public void meth1()
+	public void meth1()   //creating a method named as meth1 
 	{
 		System.out.println("Hello Everyone");
 	}
