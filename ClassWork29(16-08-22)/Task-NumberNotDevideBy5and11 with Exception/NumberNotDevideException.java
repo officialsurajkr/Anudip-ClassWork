356f5b4102675b544685610165e4adb8be27f64a
Suraj Kumar <77854684+officialsurajkr@users.noreply.github.com>
2022-08-22 14:14:54 +0530

    Add files via upload

---
+++ ClassWork29(16-08-22)/Task-NumberNotDevideBy5and11 with Exception/NumberNotDevideException.java
@@ -0,0 +1,15 @@
+package exceptionhandling;
+
+//unchecked exception
+public class NumberNotDevideException extends RuntimeException
+{
+	
+		public NumberNotDevideException (String message)
+		{
+			super(message);
+		}
+
+	}
+
+
+
