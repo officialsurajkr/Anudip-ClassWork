2e500597039295330ca4f7d32361c8d347be080e
Suraj Kumar <77854684+officialsurajkr@users.noreply.github.com>
2022-08-31 17:32:14 +0530

    Create Employee.java

---
+++ ClassWork38(31-08-22)/Employee.java
@@ -0,0 +1,36 @@
+package collectionframework;
+
+public class Employee
+{
+	private int empId;
+	private String empName;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	public void display()
+	{
+		System.out.println("Emp Id: "+ getEmpId()+ " Emp Name: "+ getEmpName());
+	}
+	
+	
+	
+	
+
+}
